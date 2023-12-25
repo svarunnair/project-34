11a9c1b5441e6dfbe0d6a28fc3229650faf11888
Varun Nair <varunsnair1997@gmail.com>
2023-12-26 00:19:09 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import PublicRoutes from './Routes/PublicRoutes';
 import PrivateRoutes from './Routes/PrivateRoutes';
+import Navbar from './Pages/Navbar';
 
 function App() {
   let token=localStorage.getItem("token")
   let main=token?<PrivateRoutes/>:<PublicRoutes/>
   return (
     <div className="App">
-{main}
+{/* {main} */}
+<Navbar/>
     </div>
   );
 }
