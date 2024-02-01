60894a7d8295aefea7f93686b6835582023e34a5
Varun Nair <varunsnair1997@gmail.com>
2024-02-02 00:08:43 +0530

    warrenty

--- src/Pages/Warrenty.jsx
+++ src/Pages/Warrenty.jsx
@@ -1,8 +1,25 @@
+import { Box, styled } from '@mui/material';
 import React from 'react'
+
+
+const OuterContainer = styled(Box)(({ theme }) => ({
+  border: "2px solid green",
+ 
+
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {},
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.down("xs")]: {},
+}));
 
 function Warrenty() {
   return (
-    <div></div>
+    <OuterContainer>
+      
+
+
+    </OuterContainer>
   )
 }
 
