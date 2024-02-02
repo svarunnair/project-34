4d47c78185fc7133e736df18612f76f7a507894b
Varun Nair <varunsnair1997@gmail.com>
2024-02-03 00:18:58 +0530

    warrenty

--- src/Pages/Warrenty.jsx
+++ src/Pages/Warrenty.jsx
@@ -12,11 +12,22 @@ const OuterContainer = styled(Box)(({ theme }) => ({
   [theme.breakpoints.down("sm")]: {},
   [theme.breakpoints.down("xs")]: {},
 }));
+const InnerDiv = styled(Box)(({ theme }) => ({
+  border: "2px solid green",
+ 
+
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {},
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.down("xs")]: {},
+}));
 
 function Warrenty() {
   return (
     <OuterContainer>
-      
+      <InnerDiv></InnerDiv>
+
 
 
     </OuterContainer>
